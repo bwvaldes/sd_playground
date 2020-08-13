73dd7d40fe832e1ba22c7ae88bd58db9d08c1fec
Brian Valdes <bwvaldes@brians-mbp.lan>
2020-08-13 09:04:40 -0700

    Logbook Entry Baseline

--- app/src/main/java/com/scubadeving/sd_playground/ui/logbook/LogbookEntryFragment.kt
+++ app/src/main/java/com/scubadeving/sd_playground/ui/logbook/LogbookEntryFragment.kt
@@ -11,13 +11,34 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
+import androidx.recyclerview.widget.SnapHelper
 import com.scubadeving.sd_playground.R
+import com.scubadeving.sd_playground.ui.adapters.recyclerview.DiveCenterAdapter
+import com.scubadeving.sd_playground.ui.adapters.recyclerview.ItemDetailAdapter
+import com.scubadeving.sd_playground.ui.adapters.recyclerview.WildlifeAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_dive_site_detail.*
 import kotlinx.android.synthetic.main.fragment_logbook_entry.*
+import kotlinx.android.synthetic.main.fragment_logbook_entry.wildlife_rv
 
 
 class LogbookEntryFragment : Fragment() {
 
+    private var wildLife: List<String> =
+        listOf(
+            "Garibaldi",
+            "Halibut",
+            "Horn Shark",
+            "Sheephead",
+            "Bat Ray",
+            "Blennies",
+            "Moray Eel"
+        )
+    private lateinit var logbookEntryWildlifeLayoutManager: LinearLayoutManager
+    private lateinit var logbookEntryWildlifeAdapter: WildlifeAdapter
     private lateinit var logbookEntryViewModel: LogbookEntryViewModel
 
     override fun onCreateView(
@@ -55,5 +76,20 @@ class LogbookEntryFragment : Fragment() {
                 .setPositiveButton("Done", null)
                 .show()
         }
+        configureWildlife()
+    }
+    private fun configureWildlife() {
+        logbookEntryWildlifeLayoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        wildlife_rv.layoutManager = logbookEntryWildlifeLayoutManager
+        logbookEntryWildlifeAdapter = WildlifeAdapter(wildLife)
+        wildlife_rv.adapter = logbookEntryWildlifeAdapter
+        val dividerItemDecoration = DividerItemDecoration(
+            wildlife_rv.context,
+            logbookEntryWildlifeLayoutManager.orientation
+        )
+        wildlife_rv.addItemDecoration(dividerItemDecoration)
+        val snapHelper: SnapHelper = PagerSnapHelper()
+        snapHelper.attachToRecyclerView(wildlife_rv)
     }
 }
