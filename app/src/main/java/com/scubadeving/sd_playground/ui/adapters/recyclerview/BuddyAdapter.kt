a4b915a5ecb5402a0834ea4b28c844b5496c936c
Brian Valdes <bwvaldes@brians-mbp.lan>
2020-08-15 10:06:22 -0700

    Baseline for Buddies

---
+++ app/src/main/java/com/scubadeving/sd_playground/ui/adapters/recyclerview/BuddyAdapter.kt
@@ -0,0 +1,61 @@
+package com.scubadeving.sd_playground.ui.adapters.recyclerview
+
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.scubadeving.sd_playground.R
+import com.scubadeving.sd_playground.utils.inflate
+import kotlinx.android.synthetic.main.item_explore_buddies_horizontal_card.view.*
+import kotlinx.android.synthetic.main.item_explore_buddies_vertical_card.view.*
+
+class BuddyAdapter(private val divers: List<String>, val orientation: Boolean) :
+    RecyclerView.Adapter<BuddyAdapter.BuddyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BuddyViewHolder {
+        val inflatedView = if (orientation) {
+            parent.inflate(R.layout.item_explore_buddies_horizontal_card, false)
+        } else {
+            parent.inflate(R.layout.item_explore_buddies_vertical_card, false)
+
+        }
+        return BuddyViewHolder(inflatedView)
+    }
+
+    override fun getItemCount(): Int = divers.size
+
+    override fun onBindViewHolder(holder: BuddyViewHolder, position: Int) {
+        val details = divers[position]
+        holder.bind(details, position)
+    }
+
+    inner class BuddyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),
+        View.OnClickListener {
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View) {
+            Log.d("RecyclerView", "CLICK!")
+            Toast.makeText(
+                itemView.context,
+                "Just Clicked Buddy Card!",
+                Toast.LENGTH_SHORT
+            )
+                .show()
+        }
+
+        fun bind(diver: String, position: Int) {
+            itemView.apply {
+                if (orientation) {
+                    nearby_diver_text.text = diver
+                } else {
+                    diver_text.text = diver
+                }
+            }
+        }
+
+    }
+}
--- app/src/main/java/com/scubadeving/sd_playground/ui/main/explore/buddies/ExploreBuddiesFragment.kt
+++ app/src/main/java/com/scubadeving/sd_playground/ui/main/explore/buddies/ExploreBuddiesFragment.kt
@@ -8,10 +8,52 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.*
 import com.scubadeving.sd_playground.R
+import com.scubadeving.sd_playground.decorations.GridSpacingItemDecoration
+import com.scubadeving.sd_playground.ui.adapters.recyclerview.BuddyAdapter
+import com.scubadeving.sd_playground.ui.adapters.recyclerview.ItemDetailAdapter
+import com.scubadeving.sd_playground.ui.adapters.recyclerview.SpecialtyAdapter
+import kotlinx.android.synthetic.main.fragment_detail_cert.*
+import kotlinx.android.synthetic.main.fragment_explore_buddies.*
+import kotlinx.android.synthetic.main.fragment_profile_certifications.*
 
 class ExploreBuddiesFragment : Fragment() {
 
+    private var nearbyDivers: List<String> =
+        listOf(
+            "Bob",
+            "Billy",
+            "Jill",
+            "Karen",
+            "Molly",
+            "Don",
+            "Bill",
+            "Greg"
+        )
+
+    private var diveCenterDivers: List<String> =
+        listOf(
+            "Karen",
+            "Molly",
+            "Chad",
+            "Stuart"
+        )
+
+
+    private var pastDivers: List<String> =
+        listOf(
+            "Mike",
+            "Don",
+            "Bill",
+            "Greg"
+        )
+    private lateinit var nearbyDiversLayoutManager: LinearLayoutManager
+    private lateinit var diveCenterDiversLayoutManager: GridLayoutManager
+    private lateinit var pastDiversDiversLayoutManager: GridLayoutManager
+    private lateinit var nearbyDiversAdapter: BuddyAdapter
+    private lateinit var diveCenterDiversAdapter: BuddyAdapter
+    private lateinit var pastDiversDiversAdapter: BuddyAdapter
     private lateinit var exploreBuddiesViewModel: ExploreBuddiesViewModel
 
     override fun onCreateView(
@@ -31,6 +73,59 @@ class ExploreBuddiesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        configureNearbyDivers()
+        configureDiveCenterDivers()
+        configurePastDivers()
+    }
+
+    fun configureNearbyDivers() {
+        nearbyDiversLayoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        nearby_divers_rv.layoutManager = nearbyDiversLayoutManager
+        nearbyDiversAdapter = BuddyAdapter(nearbyDivers, true)
+        nearby_divers_rv.adapter = nearbyDiversAdapter
+        val dividerItemDecoration = DividerItemDecoration(
+            nearby_divers_rv.context,
+            nearbyDiversLayoutManager.orientation
+        )
+        nearby_divers_rv.addItemDecoration(dividerItemDecoration)
+        val snapHelper: SnapHelper = PagerSnapHelper()
+        snapHelper.attachToRecyclerView(nearby_divers_rv)
+    }
+
+    private fun configureDiveCenterDivers() {
+        val spanCount = 2
+        val spacing = 15
+        val includeEdge = true
+        diveCenterDiversLayoutManager =
+            GridLayoutManager(context, spanCount, GridLayoutManager.VERTICAL, false)
+        dive_center_divers_rv.layoutManager = diveCenterDiversLayoutManager
+        diveCenterDiversAdapter = BuddyAdapter(diveCenterDivers, false)
+        dive_center_divers_rv.adapter = diveCenterDiversAdapter
+        dive_center_divers_rv.addItemDecoration(
+            GridSpacingItemDecoration(
+                spanCount,
+                spacing,
+                includeEdge
+            )
+        )
+        val snapHelper: SnapHelper = PagerSnapHelper()
+        snapHelper.attachToRecyclerView(dive_center_divers_rv)
+    }
+
+
+    private fun configurePastDivers() {
+        val spanCount = 2
+        val spacing = 15
+        val includeEdge = true
+        pastDiversDiversLayoutManager =
+            GridLayoutManager(context, spanCount, GridLayoutManager.VERTICAL, false)
+        past_divers_rv.layoutManager = pastDiversDiversLayoutManager
+        pastDiversDiversAdapter = BuddyAdapter(pastDivers, false)
+        past_divers_rv.adapter = pastDiversDiversAdapter
+        past_divers_rv.addItemDecoration(GridSpacingItemDecoration(spanCount, spacing, includeEdge))
+        val snapHelper: SnapHelper = PagerSnapHelper()
+        snapHelper.attachToRecyclerView(past_divers_rv)
     }
 
 
