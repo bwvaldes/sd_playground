a14d0f76f78b57576418e24d5ce63ac1b5f8e82b
Brian Valdes <bwvaldes@brians-mbp.lan>
2021-02-07 00:46:04 -0800

    Modify Dive Site Card

--- app/src/main/java/com/scubadeving/sd_playground/ui/adapters/recyclerview/DiveSiteAdapter.kt
+++ app/src/main/java/com/scubadeving/sd_playground/ui/adapters/recyclerview/DiveSiteAdapter.kt
@@ -1,18 +1,21 @@
 package com.scubadeving.sd_playground.ui.adapters.recyclerview
 
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.os.Build
 import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.scubadeving.sd_playground.R
 import com.scubadeving.sd_playground.utils.inflate
-import kotlinx.android.synthetic.main.item_cert_card.view.cert_card_text
+import kotlinx.android.synthetic.main.item_dive_site_card.view.*
 
 class DiveSiteAdapter(private val diveSites: List<String>) :
     RecyclerView.Adapter<DiveSiteAdapter.DiveSiteViewHolder>() {
-
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DiveSiteViewHolder {
@@ -41,9 +44,27 @@ class DiveSiteAdapter(private val diveSites: List<String>) :
             v.findNavController().navigate(R.id.diveSiteDetailFragment)
         }
 
+        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
         fun bind(diveSite: String, position: Int) {
             itemView.apply {
-                cert_card_text.text = diveSite
+                dive_site_card_favorite.apply {
+                    setOnCheckedChangeListener { _, isFavorite ->
+                        // TODO: Magic
+                    }
+                }
+                when (position) {
+                    1 -> backgroundTintList = ColorStateList.valueOf(Color.RED)
+                    2 -> backgroundTintList = ColorStateList.valueOf(Color.MAGENTA)
+                    3 -> {
+                        backgroundTintList = ColorStateList.valueOf(Color.GREEN)
+                        dive_site_card_popularity.visibility = View.VISIBLE
+                    }
+                }
+                val ratingValue = "4.5"
+                val reviewCount = " (23)"
+                dive_site_card_rating.text = ratingValue.plus(reviewCount)
+                dive_site_card_name.text = diveSite
+                dive_site_card_location.text = "California"
             }
         }
 
